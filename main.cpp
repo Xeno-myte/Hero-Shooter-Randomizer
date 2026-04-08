e3fcc0237927a4fc54265ac18f154719164b4d5d
Xeno-myte <51387885+Xeno-myte@users.noreply.github.com>
2026-04-07 20:11:33 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <ctime>
+#include<windows.h>
+
+
+
+
+int main() {
+
+
+int tank = 1;
+int dps = 1;
+int supp = 1;
+
+int number;
+int characters;
+
+std::cout << "Welcome to the Randomizer" << '\n';
+Sleep(2000);
+
+std::cout << "Pick a number for the role you want randomized" << '\n' << '\n' << "1. Tank | 2. DPS | 3. Support" << '\n' << '\n';
+std::cin >> number;
+while(number != 1 && number != 2 && number != 3){
+    std::cout << "Please select a number from the listing." << '\n';
+    std::cout << "Pick a number for the role you want randomized" << '\n' << '\n' << "1. Tank | 2. DPS | 3. Support" << '\n'<< '\n';
+    std::cin >> number;
+}
+std::cout << "How many characters are in the role you selected?" << '\n' << '\n';
+std::cin >> characters;
+
+if(number == 1){
+    srand(time(NULL));
+    tank = (rand() % characters) + 1;
+    std::cout << "Your chosen tank is #: " << tank;
+} else if(number == 2){
+    srand(time(NULL));
+    dps = (rand() % characters) + 1;
+    std::cout << "Your chosen dps is #: " << dps;
+} else if(number == 3){
+    srand(time(NULL));
+    supp = (rand() % characters) + 1;
+    std::cout << "Your chosen support is #: " << supp << '\n';
+}
+
+
+
+system("pause");
+}
